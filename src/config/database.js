f1495de44b345aa6ccd545f06ae94abc8c80c630
kaiquecardoso <39771108+kaiquecardoso@users.noreply.github.com>
2019-02-26 09:37:29 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "mysql",
+  host: "127.0.0.1",
+  username: "root",
+  passwoord: "",
+  database: "gonodemodulo2",
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
